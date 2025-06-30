aa57f465a92efedec511ab1fa1aaf069c363e395
oluwafemi S Idiakhoa <mgbam@users.noreply.huggingface.co>
2025-06-30 22:50:55 +0000

    Update app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config_by_name
+import os
 
 db = SQLAlchemy()
 
 def create_app(config_name='dev'):
-    app = Flask(__name__)
+    # Pass the instance_path directly during app creation
+    app = Flask(__name__, instance_path=config_by_name[config_name].INSTANCE_PATH, instance_relative_config=True)
     app.config.from_object(config_by_name[config_name])
+    
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
     
     db.init_app(app)
 
