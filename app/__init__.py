d549776f985793f95f72ff376eb999658db0ee9d
oluwafemidiakhoa <38867499+oluwafemidiakhoa@users.noreply.github.com>
2025-07-01 19:06:38 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,24 +3,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import config # Import the final, chosen config object
-import os
 
 db = SQLAlchemy()
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object(config)
+def create_app(config_name='dev'):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(config[config_name])
+    
+    # Ensure the instance folder exists, as Flask needs it.
+    # We will create the db file separately.
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
     
     db.init_app(app)
 
-    # Within the app context, ensure the database and its tables are created.
-    # This will write to the /data directory specified in the config.
-    with app.app_context():
-        # Importing models here prevents circular import errors
-        from . import models
-        db.create_all()
-
-    # Import and register the blueprints for our routes
+    # Import and register blueprints
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
